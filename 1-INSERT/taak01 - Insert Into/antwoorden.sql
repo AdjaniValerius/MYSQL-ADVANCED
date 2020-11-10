05e3812118e36f6d19d221fa119aebb40165dd6f
Adjani Valerius <2068687@talnet.nl>
2020-11-10 11:36:12 +0100

    mySQL Advanced-1- taak1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,20 @@
 
+-- ANTWOORDEN CODE:
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES (5, 'Jan', 'Piet', '9a', 'geertstraat 12', '4785DE', 2001-08-14, 'Amsterdam' );
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES (6, 'Klaas', 'Hendrick', '8c', 'surinameplein 64', '1782GK', 2000-12-15, 'Alkmaar' );
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES (7, 'frank', 'Gal', '7d', 'hekplein 78', '3957NB', 1998-05-04, 'Amsterdam' );
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES (8, 'Iran', 'Jongen', '2z', 'plein40/45', '0028QA', 2004-02-07, 'Leiden' );
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES (9, 'Piet', 'Steen', '3i', 'amstelveenseweg D2', '1122BB', 2003-04-31, 'Rotterdam' );
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES (10, 'Loes', 'Appel', 'ZQ', 'blokkenpark', '9947BR', 2003-09-29, 'Amstelveen' );
