d595b361dfc7fb0a80534d261549d8a47653bd24
Adjani Valerius <2068687@talnet.nl>
2020-11-10 12:29:05 +0100

    mySQL-1-taak2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,21 @@
+-- OPDRACHT 1
 
+INSERT INTO studenten (voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES ('Angelo', 'pieterdom', '9b', 'stoelstraat 45', '1012EE', 2001-01-01, 'Amsterdam' );
+
+INSERT INTO studenten (voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES ('lizzie', 'steenderen', '4g', 'tafelplein 77', '8844FF', 1999-12-17, 'Alkmaar' );
+
+INSERT INTO studenten (voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES ('sanne', 'bloem', '5p', 'deurstraat 01', '0007QQ', 1998-05-01, 'Rotterdm' );
+
+-- OPDRACHT 2  
+
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) 
+VALUES ('Hallen', 'Wijk', 2001-02-02, 'Amsterdam' );
+
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) 
+VALUES ('Hnedrika', 'Jansen', 1999-11-16, 'Alkmaar' );
+
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) 
+VALUES ('emanuel', 'obia', 1998-06-02, 'Rotterdm' );
